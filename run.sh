cbc856a0f2df10955ed76f3b4d866116a8463e3a
MitrichevGeorge <131480793+MitrichevGeorge@users.noreply.github.com>
2025-10-02 19:31:07 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,18 +11,18 @@ REMOTE_VER=$(curl -fsSL "$REMOTE_VER_URL")
 if [ "$LOCAL_VER" != "$REMOTE_VER" ]; then
   MAIN_PID=$$
   TMPFILE=$(mktemp /tmp/myscript.XXXXXX.sh)
-  
-  cat > "$TMPFILE" <<EOF
-  #!/bin/sh
-  while kill -0 $MAIN_PID 2>/dev/null; do
-      sleep 0.01
-  done
 
-  rm ~/.mserv
-  curl -fsSL https://raw.githubusercontent.com/MitrichevGeorge/mserv/main/install.sh | bash
-  
-  rm -- "\$0"
-  EOF
+cat > "$TMPFILE" <<EOF
+#!/bin/sh
+while kill -0 $MAIN_PID 2>/dev/null; do
+    sleep 0.01
+done
+
+rm ~/.mserv
+curl -fsSL https://raw.githubusercontent.com/MitrichevGeorge/mserv/main/install.sh | bash
+
+rm -- "\$0"
+EOF
   
   chmod +x "$TMPFILE"
   
